bdc84939c8d40322d692481c7a372765f841acd8
VishSeran <vishwa.sovis@gmail.com>
2026-07-18 12:44:57 +0530

    document processor- validate file updated

--- configuration/config.py
+++ configuration/config.py
@@ -1,1 +1,2 @@
 CACHE_DIR = "../vectordb/document_cache"
+MAX_FILE_SIZE = 50
--- vectordb/file_handler.py
+++ vectordb/file_handler.py
@@ -2,6 +2,7 @@ from configuration.logger import get_logger
 from docling.document_converter import DocumentConverter
 from configuration.config import CACHE_DIR
 from pathlib import Path
+import os
 
 logger = get_logger("file-handler")
 
@@ -12,5 +13,23 @@ class DocumentProcessor:
         self.cache_dir = Path(CACHE_DIR)
         self.cache_dir.mkdir(parents=True, exist_ok=True)
         
+    def valiate_file(self, files:list):
+        
+        try:
+            
+            if not files:
+                raise ValueError("Files cannot be empty")
+            
+            file_size = sum(os.path.getsize(file.name) for file in files)
+            
+            if file_size > 
+            
+        except ValueError as e:
+            logger.error(f"Value error: {e}")
+            raise
+        
+        except Exception as e:
+            logger.error(f"Error in document processor: {e}")
+            raise
         
     
