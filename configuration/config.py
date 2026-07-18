9281c0b15796759ab80d3d8155f756958d011fed
VishSeran <vishwa.sovis@gmail.com>
2026-07-18 16:22:49 +0530

    vector build- init updated

--- configuration/config.py
+++ configuration/config.py
@@ -2,3 +2,4 @@ CACHE_DIR = "../vectordb/document_cache"
 MAX_FILE_SIZE = 50
 EMBED_MODEL = "BAAI/bge-m3"
 COLLECTION_NAME = "docchat_collection"
+CHROMA_DB_PATH = "../vectordb/vectore_store"
--- vectordb/vector_build.py
+++ vectordb/vector_build.py
@@ -1,13 +1,16 @@
 
 from langchain_huggingface import HuggingFaceEmbeddings
 from configuration.logger import get_logger
+from configuration.config import COLLECTION_NAME
+from langchain_community.vectorstores import Chroma
+
 
 
 logger = get_logger("vector-build")
 
 class VectorBuild:
-    
-    def __init__(self):
+     
+    def __init__(self, docs, collection_name:str = COLLECTION_NAME):
         
         self.embedding_model = HuggingFaceEmbeddings(
             model_name = "BAAI/bge-m3",
@@ -17,6 +20,13 @@ class VectorBuild:
             encode_kwargs = {
                 "normalize_embeddings": True
             }
+        )
+        
+        self.vector_store = Chroma.from_documents(
+            documents=docs,
+            embedding=self.embedding_model,
+            collection_name=collection_name,
+            pers
         )
         
         
