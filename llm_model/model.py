fa67f9be9f818bd1ad7cdb9411a7423a4f3e3283
VishSeran <vishwa.sovis@gmail.com>
2026-07-19 11:57:58 +0530

    llm model - init updated

--- llm_model/model.py
+++ llm_model/model.py
@@ -1,7 +1,7 @@
-
 from langchain_groq import ChatGroq
 from configuration.logger import get_logger
 from dotenv import load_dotenv
+from configuration.config import EVALUATOR_MODEL, LLM_MODEL 
 import os
 
 
@@ -9,22 +9,25 @@ logger = get_logger("model")
 
 class LLMModel:
     
-    def __int__(self, model_id):
+    def __int__(self, evaluator_model = EVALUATOR_MODEL, llm_model_id = LLM_MODEL):
         
         try:
             
-        
             load_dotenv()
             GROQ_API = os.getenv("GROQ_API")
             
+            if not GROQ_API:
+                raise ValueError("groq api key is missing")
+            
+            
             self.llm_grader =  ChatGroq(
-                model=model_id,
+                model=evaluator_model,
                 temperature=0,
                 api_key=GROQ_API
             )
             
             self.llm_generator = ChatGroq(
-                model=model_id,
+                model=llm_model_id,
                 temperature=0.5,
                 api_key=GROQ_API
             )
@@ -36,3 +39,8 @@ class LLMModel:
         except Exception as e:
             logger.error(f"Error in llm model initialization : {e}")
             raise
+        
+    
+    def get_evaluator_chain (self):
+        
+        
