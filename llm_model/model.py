94048b909e95801f23e934de98f3bd10f1c5be0a
VishSeran <vishwa.sovis@gmail.com>
2026-07-19 12:10:57 +0530

    llm model - get evalutor chain updated

--- llm_model/model.py
+++ llm_model/model.py
@@ -1,7 +1,8 @@
 from langchain_groq import ChatGroq
 from configuration.logger import get_logger
 from dotenv import load_dotenv
-from configuration.config import EVALUATOR_MODEL, LLM_MODEL 
+from configuration.config import EVALUATOR_MODEL, LLM_MODEL, relevance_checker_prompt
+from langchain_core.prompts import PromptTemplate
 import os
 
 
@@ -23,14 +24,17 @@ class LLMModel:
             self.llm_grader =  ChatGroq(
                 model=evaluator_model,
                 temperature=0,
+                max_tokens=10,
                 api_key=GROQ_API
             )
             
             self.llm_generator = ChatGroq(
                 model=llm_model_id,
                 temperature=0.5,
+                max_tokens=200,
                 api_key=GROQ_API
             )
+            
         
         except ValueError as e:
             logger.error(f"Value error: {e}")
@@ -43,4 +47,31 @@ class LLMModel:
     
     def get_evaluator_chain (self):
         
+        """
+        1. Retrieve the top-k document chunks from the global retriever.
+        2. Combine them into a single text string.
+        3. Pass that text + question to the LLM for classification.        
         
+        Returns: "CAN_ANSWER", "PARTIAL", or "NO_MATCH".
+        """
+        
+        try:
+            
+            prompt_template = PromptTemplate(
+                template=relevance_checker_prompt,
+                input_variables=["question","document_content"],
+                
+            )
+            
+            relevance_chain = prompt_template | self.llm_grader
+            
+            return relevance_chain
+            
+            
+        except ValueError as e:
+            logger.error(f"Value error: {e}")
+            raise
+        
+        except Exception as e:
+            logger.error(f"Error in get evaluator chain initialization : {e}")
+            raise
