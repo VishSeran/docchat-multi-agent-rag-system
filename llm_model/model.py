cb5f0bebce90bee5a374aa7d2df0ab30cf250e8a
VishSeran <vishwa.sovis@gmail.com>
2026-07-20 10:17:00 +0530

    verifier evaluator - init fn updated

--- llm_model/model.py
+++ llm_model/model.py
@@ -21,13 +21,6 @@ class LLMModel:
                 raise ValueError("groq api key is missing")
             
             
-            self.llm_grader =  ChatGroq(
-                model=evaluator_model,
-                temperature=0,
-                max_tokens=10,
-                api_key=GROQ_API
-            )
-            
             self.llm_generator = ChatGroq(
                 model=llm_model_id,
                 temperature=0.5,
@@ -35,28 +28,7 @@ class LLMModel:
                 api_key=GROQ_API
             )
             
-            relevence_prompt_template = ChatPromptTemplate.from_messages(
-                [
-                    (
-                        "system",
-                        relevance_checker_prompt
-                    ),
-                    
-                    (
-                        "human",
-                        """
-                        question: 
-                        {question}
-                        
-                        document_content:
-                        {document_content}
-                         
-                        """
-                    )
-                ]
-            )
             
-            self.relevance_chain = relevence_prompt_template | self.llm_grader
             
             verifier_prompt_template = ChatPromptTemplate.from_messages(
                 [
@@ -86,35 +58,6 @@ class LLMModel:
         
         except Exception as e:
             logger.error(f"Error in llm model initialization : {e}")
-            raise
-        
-        
-    def evaluate_relevance(self, question, document_content):
-        
-        """
-        1. Retrieve the top-k document chunks from the global retriever.
-        2. Combine them into a single text string.
-        3. Pass that text + question to the LLM for classification.        
-        
-        Returns: "CAN_ANSWER", "PARTIAL", or "NO_MATCH".
-        """
-        
-        try:
-            response = self.relevance_chain.invoke({
-                "question": question,
-                "document_content": document_content
-            })
-            
-            return response.content.strip()
-            
-            
-            
-        except ValueError as e:
-            logger.error(f"Value error: {e}")
-            raise
-        
-        except Exception as e:
-            logger.error(f"Error in get evaluate relavence: {e}")
             raise
         
     
---
+++ llm_model/verifier_evaluator.py
@@ -0,0 +1,10 @@
+
+from configuration.logger import get_logger
+
+
+logger = get_logger("verifier-evalutor")
+
+class VerifierEvaluator:
+    
+    def __init__(self):
+        pass
