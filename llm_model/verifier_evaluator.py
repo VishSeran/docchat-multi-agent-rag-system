222279ba4b93c602deb0d33d8b4279c18d4ae147
VishSeran <vishwa.sovis@gmail.com>
2026-07-20 10:35:15 +0530

    verifier evaluator - verifier response fn updated

--- llm_model/verifier_evaluator.py
+++ llm_model/verifier_evaluator.py
@@ -12,39 +12,77 @@ class VerifierEvaluator:
     
     def __init__(self, verifier_model=VERIFIER_EVALUATOR_MODEL):
         
-        dotenv.load_dotenv()
-        groq_api = os.getenv("GROQ_API")
-        
-        
-        self.llm_verifier = ChatGroq(
-            model=verifier_model,
-            temperature=0,
-            max_tokens=10,
-            api_key=groq_api
-        )
-        
-        logger.info("LLM verifier is created")
-        
-        verifier_prompt_template = ChatPromptTemplate.from_messages([
-            (
-                "system",
-                verifier_prompt
-            ),
+        try:
             
-            (
-                "human",
-                """
-                answer: 
-                {answer}
-                
-                context:
-                {context}
-                         
-                """
+            if not verifier_model:
+                raise ValueError("Verifier model is required to futher process!!!")
+        
+            dotenv.load_dotenv()
+            groq_api = os.getenv("GROQ_API")
+            
+            
+            self.llm_verifier = ChatGroq(
+                model=verifier_model,
+                temperature=0,
+                max_tokens=10,
+                api_key=groq_api
             )
             
-        ])
+            logger.info("LLM verifier is created")
+            
+            verifier_prompt_template = ChatPromptTemplate.from_messages([
+                (
+                    "system",
+                    verifier_prompt
+                ),
+                
+                (
+                    "human",
+                    """
+                    answer: 
+                    {answer}
+                    
+                    context:
+                    {context}
+                            
+                    """
+                )
+                
+            ])
+            
+            logger.info("LLM verifier prompt is created")
+            self.verifier_chain = verifier_prompt_template | self.llm_verifier
+            logger.info("LLM verifier chain is created successful")
         
-        logger.info("LLM verifier prompt is created")
-        self.verifier_chain = verifier_prompt_template | self.llm_verifier
-        logger.info("LLM verifier chain is created successful")
+        except ValueError as e:
+            logger.error(f"Value error: {e}")
+            raise
+        
+        except Exception as e:
+            logger.error(f"Error in verifier evaluator initialization: {e}")
+            raise
+        
+    
+    def verifier_response (self, answer, context):
+        
+        try:
+            
+            if not answer:
+                raise ValueError("Model answer is missing")
+            
+            if not context:
+                raise ValueError("Retrieved contexts are missing")
+            
+            response = self.verifier_chain.invoke({
+                "answer": answer,
+                "context": context
+            })
+        
+        except ValueError as e:
+            logger.error(f"Value error: {e}")
+            raise
+        
+        except Exception as e:
+            logger.error(f"Error in verifier response: {e}")
+            raise   
+        
