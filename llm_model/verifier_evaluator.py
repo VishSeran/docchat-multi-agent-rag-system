6db427e32e6456dd70f4c522aea35e3faaa91ba1
VishSeran <vishwa.sovis@gmail.com>
2026-07-20 10:23:16 +0530

    verifier evaluator - init fn updated

--- llm_model/verifier_evaluator.py
+++ llm_model/verifier_evaluator.py
@@ -1,10 +1,44 @@
-
+from configuration.config import VERIFIER_EVALUATOR_MODEL, verifier_prompt
 from configuration.logger import get_logger
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+import dotenv
+import os
 
 
 logger = get_logger("verifier-evalutor")
 
 class VerifierEvaluator:
     
-    def __init__(self):
-        pass
+    def __init__(self, verifier_model=VERIFIER_EVALUATOR_MODEL):
+        
+        dotenv.load_dotenv()
+        groq_api = os.getenv("GROQ_API")
+        
+        
+        self.llm_verifier = ChatGroq(
+            model=verifier_model,
+            temperature=0,
+            max_tokens=10,
+            api_key=groq_api
+        )
+        
+        verifier_prompt = ChatPromptTemplate.from_messages([
+            (
+                "system",
+                verifier_prompt
+            ),
+            
+            (
+                "human",
+                """
+                answer: 
+                {answer}
+                
+                context:
+                {context}
+                         
+                """
+            )
+            
+        ])
