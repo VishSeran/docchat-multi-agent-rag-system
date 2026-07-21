715b27bc238e0703881e44d518ed70732ac2b613
VishSeran <vishwa.sovis@gmail.com>
2026-07-21 10:23:57 +0530

    agent workflow-init updated

--- agent_workflow/agent_workflow.py
+++ agent_workflow/agent_workflow.py
@@ -1,12 +1,19 @@
-
+from agents.relavence_evaluator_agent import RelavenceEvaluatorAgent
+from agents.research_agent import ResearchAgent
+from agents.verifier_evaluator_agent import VerifierEvaluatorAgent
 from configuration.logger import get_logger
 
 
 logger = get_logger("agent-workflow")
 
-
-
-
 class AgentWorkflow:
+    
+    def __init__(self):
+        self.relavence_agent = RelavenceEvaluatorAgent()
+        self.research_agent = ResearchAgent()
+        self.verifier_agent = VerifierEvaluatorAgent()
+        logger.info("Agents are initialized")
+        
+        
     
     
