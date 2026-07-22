070cddca41aa8e3fc213402ed3fdff6fab50afde
VishSeran <vishwa.sovis@gmail.com>
2026-07-22 22:47:15 +0530

    main-gradio updated

--- main.py
+++ main.py
@@ -17,6 +17,62 @@ def main():
         vector_db = VectorBuild(embedding_model.get_model())
         workflow = AgentWorkflow()
         
+        
+        def question_handler(question:str, docs:list, state:dict):
+            
+            try:
+                
+                if not question:
+                    raise ValueError("Question must not be empty")
+                
+                if not docs:
+                    raise ValueError("Documents must not be empty")
+                
+                if not state:
+                    raise ValueError("State must not be empty")
+                
+                logger.info("Processing new/changed documents...")
+                
+                current_hashes = _get_file_hash(docs)
+                
+                if state['retriever'] is None or current_hashes != state['hashes']:
+                
+                    chunks = document_processor.document_process(docs)
+                    retriever = vector_db.get_retriever(
+                        chunks=chunks
+                    )
+                    
+                    state.update({
+                        "hashes": current_hashes,
+                        "retriever": retriever
+                    })
+                
+                result = workflow.run(
+                    question=question,
+                    retriever=state['retriever']
+                )
+                
+                return result['final_answer'], result['verifier_result'], state
+    
+            except ValueError as e:
+                    logger.error(f"Value error: {e}")   
+                    raise
+                
+            except Exception as e:
+                logger.error(f"Error in question handler: {e}")
+                return f"Error: {str(e)}", "", state 
+            
+        
+        def _get_file_hash(upload_docs:list)->frozenset:
+            
+            hashes = set()
+            
+            for file in upload_docs:
+                with open(file.name, "rb") as f:
+                    hashes.add(hashlib.sha256(f.read()).hexdigest())
+                    
+            return frozenset(hashes)
+        
         with gr.Blocks(title="DocChat - Multi Agent RAG System") as demo:
             gr.Markdown("## DocChat: powered by Docling 🐥 and LangGraph")
             gr.Markdown("# How it works ✨:")
@@ -43,76 +99,17 @@ def main():
                     verfication_result = gr.TextArea(label="✅ Verification Report")
                     
                 submit_btn.click(
-                    fn = question_handler,
-                    inputs = [question,files],
-                    outputs = [answer, verfication_result, session_state]
+                    fn=question_handler,
+                    inputs=[question,files],
+                    outputs=[answer,verfication_result, session_state]
+                    
                 )
         
-        
     except ValueError as e:
-            logger.error(f"Value error: {e}")   
-            raise
+        logger.error(f"Value error: {e}")   
+        raise
         
     except Exception as e:
         logger.error(f"Error in main: {e}")
         raise 
-    
-    
-    def question_handler(question:str, docs:list, state:dict):
-        
-        try:
-            
-            if not question:
-                raise ValueError("Question must not be empty")
-            
-            if not docs:
-                raise ValueError("Documents must not be empty")
-            
-            if not state:
-                raise ValueError("State must not be empty")
-            
-            logger.info("Processing new/changed documents...")
-            
-            current_hashes = _get_file_hash(docs)
-            
-            if state['retriever'] is None or current_hashes != state['hashes']:
-            
-                chunks = document_processor.document_process(docs)
-                retriever = vector_db.get_retriever(
-                    chunks=chunks
-                )
-                
-                state.update({
-                    "hashes": current_hashes,
-                    "retriever": retriever
-                })
-            
-            result = workflow.run(
-                question=question,
-                retriever=state['retriever']
-            )
-            
-            return result['final_answer'], result['verifier_result'], state
-   
-        except ValueError as e:
-                logger.error(f"Value error: {e}")   
-                raise
-            
-        except Exception as e:
-            logger.error(f"Error in question handler: {e}")
-            return f"Error: {str(e)}", "", state 
-        
-    
-    def _get_file_hash(upload_docs:list)->frozenset:
-        
-        hashes = set()
-        
-        for file in upload_docs:
-            with open(file.name, "rb") as f:
-                hashes.add(hashlib.sha256(f.read()).hexdigest())
-                
-        return frozenset(hashes)
-                
-        
-    
     
